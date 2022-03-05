fba8aba38181d2771c111c66cd166d6019e62171
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2022-03-05 17:47:20 +0300

    Add Publisher model and extend existing models

--- pythonlibrary/catalog/models.py
+++ pythonlibrary/catalog/models.py
@@ -9,6 +9,10 @@ class Tag(models.Model):
         help_text='Введите тег'
     )
 
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
+
     def __str__(self):
         """Строковое представление модели"""
         return self.name
@@ -20,6 +24,10 @@ class Language(models.Model):
         max_length=200,
         help_text='Введите язык книги (русский, английский)'
     )
+
+    class Meta:
+        verbose_name = 'язык'
+        verbose_name_plural = 'языки'
 
     def __str__(self):
         """Строковое представление модели"""
@@ -33,6 +41,12 @@ class Book(models.Model):
     )
     author = models.ManyToManyField(
         'Author'
+    )
+    publisher = models.ForeignKey(
+        'Publisher',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
     )
     summary = models.TextField(
         max_length=2000,
@@ -48,24 +62,35 @@ class Book(models.Model):
     )
     tag = models.ManyToManyField(
         Tag,
+        blank=True,
         help_text='Выберите теги для этой книги'
     )
     language = models.ForeignKey(
         'Language',
         on_delete=models.SET_NULL,
-        null=True
+        null=True,
+        blank=True,
+    )
+    year = models.SmallIntegerField(
+        null=True,
+        blank=True,
     )
     cover = models.ImageField(
-        null=True
+        null=True,
+        blank=True,
     )
     num_pages = models.IntegerField(
         null=True,
         blank=True,
     )
 
+    class Meta:
+        verbose_name = 'книга'
+        verbose_name_plural = 'книги'
+
     def __str__(self):
         """Строковое представление модели"""
-        return self.title
+        return f'{self.title}'
 
     def get_absolute_url(self):
         """Возвращает url для доступа к детальной записи книги"""
@@ -90,20 +115,39 @@ class Author(models.Model):
         blank=True
     )
     picture = models.ImageField(
-        null=True
+        null=True,
+        blank=True,
     )
     about = models.TextField(
         max_length=2000,
-        help_text='Об авторе'
+        help_text='Об авторе',
+        null=True,
+        blank=True,
     )
 
     class Meta:
         ordering = ['last_name', 'first_name']
+        verbose_name = 'автор'
+        verbose_name_plural = 'авторы'
 
     def __str__(self):
         """Строковое представление модели"""
-        return f'{self.last_name}, {self.first_name}'
+        return f'{self.first_name} {self.last_name}'
 
     def get_absolute_url(self):
         """Возвращает url для доступа к детальной записи автора книги"""
         return reverse('author-detail', args=[str(self.id)])
+
+
+class Publisher(models.Model):
+    name = models.CharField(
+        max_length=30
+    )
+
+    class Meta:
+        verbose_name = 'издательство'
+        verbose_name_plural = 'издательства'
+
+    def __str__(self):
+        """Строковое представление модели"""
+        return self.name
