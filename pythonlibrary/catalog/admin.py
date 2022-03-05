8b767df6771cbf54304d794e323561434039ce12
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2022-03-05 17:51:44 +0300

    Add models to admin interface

--- pythonlibrary/catalog/admin.py
+++ pythonlibrary/catalog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, Language, Publisher, Tag
+
+
+admin.site.register(Author)
+admin.site.register(Language)
+admin.site.register(Publisher)
+admin.site.register(Tag)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'display_author',
+        'publisher',
+        'year',
+        'isbn'
+    )
+    list_filter = (
+        'publisher',
+        'year'
+    )
+
+    def display_author(self, object):
+        authors = ', '.join(
+            '{} {}'.format(author.first_name, author.last_name)
+            for author in object.author.all()
+        )
+        return authors
+    
+    display_author.short_description = 'Author'
