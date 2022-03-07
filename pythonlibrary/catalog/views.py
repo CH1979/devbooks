84c1001fdeeecdb7a6897d579df7a40ad91eac3d
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2022-03-07 16:40:37 +0300

    Add views, urls and templates

--- pythonlibrary/catalog/views.py
+++ pythonlibrary/catalog/views.py
@@ -1,6 +1,25 @@
+from django.db.models import Count
 from django.views import generic
 
-from .models import Book
+from .models import Author, Book
+
 
 class BookListView(generic.ListView):
     model = Book
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+    def get_queryset(self):
+        """Дополняет кверисет количеством книг, написанных автором"""
+        queryset = Author.objects.all().annotate(book_count = Count('book'))
+        return queryset
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
