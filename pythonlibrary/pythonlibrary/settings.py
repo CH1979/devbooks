30b9988f1c2a339a827e6f3a34e88cc4076d088b
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2022-03-11 19:59:26 +0300

    Prepare for deploy

--- pythonlibrary/pythonlibrary/settings.py
+++ pythonlibrary/pythonlibrary/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uel@3jh-@b6@m#w2i1mzp9yqt5s9qtozhu9%gc7dj@%si)^w5y'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
+'django-insecure-uel@3jh-@b6@m#w2i1mzp9yqt5s9qtozhu9%gc7dj@%si)^w5y')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
