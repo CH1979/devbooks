6f22f40e4445dcf5cb2fc4d37d5a601de6703097
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2022-03-11 16:36:28 +0300

    Add index page and finishing other pages

--- pythonlibrary/catalog/urls.py
+++ pythonlibrary/catalog/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path(
+        '',
+        views.index,
+        name='index'
+    ),
     path(
         'books',
         views.BookListView.as_view(),
--- pythonlibrary/catalog/views.py
+++ pythonlibrary/catalog/views.py
@@ -1,10 +1,22 @@
 from django.db.models import Count
+from django.shortcuts import render
 from django.views import generic
 
 from .models import Author, Book
 
 
+def index(request):
+    book_list = Book.objects.all()[:6]
+    author_list = Author.objects.order_by('last_name').all()
+    context = {
+        'book_list': book_list,
+        'author_list': author_list
+    }
+    return render(request, 'index.html', context=context)
+
+
 class BookListView(generic.ListView):
+    paginate_by = 18
     model = Book
 
 
@@ -16,8 +28,8 @@ class AuthorListView(generic.ListView):
     model = Author
 
     def get_queryset(self):
-        """Дополняет кверисет количеством книг, написанных автором"""
-        queryset = Author.objects.all().annotate(book_count = Count('book'))
+        """Возвращает кверисет, отсортированный по фамилии"""
+        queryset = Author.objects.order_by('last_name').all()
         return queryset
 
 
